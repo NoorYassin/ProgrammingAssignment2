32f3b847e1fb67665eeeb70f18f58e594f0c467e
Noor Yassin Noor <noordinyasin@yahoo.com>
2017-11-20 10:16:30 +0300

    save changes

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The program gets a matrix and looks  for stored(cached) value of its. 
+## inverse. If the stored(cached) value exists it prints it. Otherwise
+## it computes the inverse and stores(caches) the value for later use.
+ 
 
-## Write a short comment describing this function
+## This function get a matrix and computes its inverse. Then stores(caches) the 
+## inverse for later use.  
 
 makeCacheMatrix <- function(x = matrix()) {
-
+              m <- NULL
+              set <- function(y){
+                x <<- y
+                m <<- NULL
+             }
+              
+          get <- function()                  #stores the matrix whose inverse is to be computed
+                    x 
+                 
+          setinverse <- function(solve)     #computes the inverse and stores(caches) the value
+                   m <<- solve(x)
+                 
+          getinverse <- function()          
+                    m
+                  
+          
+          list = list(set = set,
+                  get = get,
+                  setinverse = setinverse,
+                  getinverse = getinverse )
 }
 
 
-## Write a short comment describing this function
+
+
+## This function looks for an inverse of a matrix if stored(cached) 
+##  if the value is not available, it calculates and stores it.  
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+          m <- x$getinverse()                    #calls from the list created in makeCacheMatrix
+          if(!is.null(m)){                       #check if inverse has been calculated
+            message("getting cached data")
+            return(m)                            #return inverse from cache if available
+          }
+          data1 <- x$get()
+          m <- solve(data1, ...)                 #computes inverse if not stored
+          x$setinverse(m)                        #caches(stores) the newly computed inverse
+          return(m)                             #return newly computed inverse
+        
 }
+
